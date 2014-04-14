7bd23a2f0fa8377801db904aa6052a35f3cfd605
Neeraj Wahi <neeraj.wahi@gmail.com>
2014-04-14 15:15:29 -0400

    Quickly added a server logfile

--- server/app.js
+++ server/app.js
@@ -12,6 +12,8 @@ var wss = new WebSocketServer({port: port});
 if(!process.env.NODE_ENV !== 'production') {
 	logger.remove(logger.transports.Console);
 	logger.add(logger.transports.Console, {'level': 'debug'});
+} else {
+	logger.add(logger.transports.File, {'level': 'debug', filename: 'pairjam_prod.log'});
 }
 
 var server = new Server();
